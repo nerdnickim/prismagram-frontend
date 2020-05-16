5bcee0c4eaafca50babf563f3cbf938a9dd5a6c8
Nerdnickk <56470910+Nerdnickk@users.noreply.github.com>
2020-05-16 17:31:55 +0900

    Global Styles Theme

---
+++ src/Styles/Theme.js
@@ -0,0 +1,18 @@
+const BOX_BORDER = "1px solid #e6e6e6e";
+const BORDER_RADIUS = "4px";
+
+export default {
+	bgColor: "#FAFAFA",
+	blackColor: "#262626",
+	darkGreyColor: "#999",
+	lightGreyColor: "#c7c7c7",
+	redColor: "#ED4956",
+	blueColor: "#3897f0",
+	darakBlueColor: "#003569",
+	boxBorder: "1px solid #e6e6e6e",
+	borderRadius: "4px",
+	whiteBox: `${BORDER_RADIUS};
+                ${BOX_BORDER};
+                background-color: white;
+                `,
+};
